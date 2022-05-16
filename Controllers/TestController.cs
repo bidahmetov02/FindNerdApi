859fa90b17a9ae9da28f50150c386ae6904ef209
Bekzhan <bidahmetov2002@gmail.com>
2022-05-16 22:56:30 +0200

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FindNerdApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Testing...";
+        }
+    }
+}
